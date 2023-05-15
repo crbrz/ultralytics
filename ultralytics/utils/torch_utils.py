91ffd0a96cfaad8720310f09894a732743fc0c0f
crbrz <cristiab@gmail.com>
2023-05-15 15:14:46 +0300

    Gloo backend doesn't support device_ids for barrier
    Signed-off-by: crbrz <cristianb@gmail.com>

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -33,10 +33,10 @@ def torch_distributed_zero_first(local_rank: int):
     """Decorator to make all processes in distributed training wait for each local_master to do something."""
     initialized = torch.distributed.is_available() and torch.distributed.is_initialized()
     if initialized and local_rank not in (-1, 0):
-        dist.barrier(device_ids=[local_rank])
+        dist.barrier(device_ids=[local_rank] if dist.is_nccl_available() else None)
     yield
     if initialized and local_rank == 0:
-        dist.barrier(device_ids=[0])
+        dist.barrier(device_ids=[0] if dist.is_nccl_available() else None)
 
 
 def smart_inference_mode():
